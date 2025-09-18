c333c9638c9c13e4afb52759abec88a0a1e4e713
Kanashii-Kito <ERicc468@nycstudents.net>
2025-09-18 10:16:22 -0400

    Update Test.py

--- Test.py
+++ Test.py
@@ -7,9 +7,70 @@
 
 
 
-x=3
-y = float(3)
-print(x,y)
-bill=x
-tip=x*0.23
+
+# y = float(3)
+
+
+
+# bill = input("How much is the Bill?")
+# bill = float(bill)
+# bill= input
+# if bill >= 0.00:
+#     tip=bill*.205
+
+# total= tip + bill
+# print(total)
+
+
+
+
+
+
+
+
+
+bill= input("How much is the bill? ")
+bill=float(bill)
+tip=bill*0.23
+print(str("Your Tip to add to the Total Bill is: ")+str(tip))
 total= tip + bill
+
+print(str("Your Total Bill with Tip is: ")+str(total))
+
+
+
+# print("Your Total Bill with tip is")
+# print(total)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+""" # x = "Nathen"
+# print(x)
+# y = input("What is your name?")
+# print(y) """
+
+# num1 = 10
+# num2 = 2.5
+# product_variables = num1 * num2
+
+
+# def add(x,y):
+#     #return is output
+#     return(x+y)
+# bill=add(5,10)
+# print(bill)
+
+
+
